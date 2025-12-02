e1945f341be89c52c1c600812a07a03c91f37169
ibrahimjuraev <juraevibrahim01@mail.ru>
2025-12-02 18:15:31 +0500

    Добавили кнопку для изменение темы

--- src/js/main.js
+++ src/js/main.js
@@ -103,3 +103,29 @@ buttons.forEach(btn => {
         }
     });
 });
+
+
+// Переключатель
+
+const handler = document.querySelector('div.toogle');
+const nameHandler = document.getElementById('handle');
+
+handler.addEventListener("click", () => {
+    if(nameHandler.textContent == 'sunny') {
+        nameHandler.textContent = 'bedtime';
+        document.documentElement.style.setProperty("--bacgroun-color", "#1e1e1e");
+        document.documentElement.style.setProperty("--arif-symbol", "#ffb54a");
+        document.documentElement.style.setProperty("--numbers", "#f1f1f1");
+        document.documentElement.style.setProperty("--hover", "#2a2a2a");
+        document.documentElement.style.setProperty("--active", "#333333");
+        document.documentElement.style.setProperty("--box", "#2c2c2c");
+    }
+    else {
+        nameHandler.textContent = 'sunny';
+        document.documentElement.style.setProperty("--bacgroun-color", "rgba(230, 230, 230, 0.747)");
+        document.documentElement.style.setProperty("--numbers", "black");
+        document.documentElement.style.setProperty("--hover", "rgb(255, 255, 255)");
+        document.documentElement.style.setProperty("--active", "rgba(145, 132, 132, 0.747)");
+        document.documentElement.style.setProperty("--box", "rgb(255, 255, 255)");
+    }
+})
